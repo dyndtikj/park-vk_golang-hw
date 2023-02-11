7d918bca32aebc5f38ebc411661ecdf133d2d4ae
dyndtikj <vlamaster08@gmail.com>
2023-02-11 11:14:16 +0300

    Add correct io

--- cmd/main.go
+++ cmd/main.go
@@ -12,7 +12,12 @@ import (
 
 const correctOpts = "go run main.go [-c | -d | -u] [-i] [-f num] [-s chars] [input_file [output_file]]"
 
-func parseCmd() (options uniq.Options) {
+type ioSettings struct {
+	input  string
+	output string
+}
+
+func parseCmd() (options uniq.Options, ioSet ioSettings) {
 	flag.BoolVar(&options.CountEntries, "c", false, "подсчитать количество встречаний строки во входных данных")
 	flag.BoolVar(&options.OnlyRepeating, "d", false, "вывести только те строки, которые повторились во входных данных.")
 	flag.BoolVar(&options.OnlyUnique, "u", false, "вывести только те строки, которые не повторились во входных данных.")
@@ -20,51 +25,83 @@ func parseCmd() (options uniq.Options) {
 	flag.IntVar(&options.IgnoreChars, "s", 0, "не учитывать первые num_chars символов в строке")
 	flag.BoolVar(&options.IgnoreRegister, "i", false, "не учитывать регистр букв")
 	flag.Parse()
-	return options
+	if flag.NArg() > 0 {
+		ioSet.input = flag.Arg(0)
+	}
+	if flag.NArg() > 1 {
+		ioSet.output = flag.Arg(1)
+	}
+	return
 }
 
-func readLines(reader io.Reader) ([]string, error) {
-	result := make([]string, 0)
+func readData(settings ioSettings) (lines []string, err error) {
+	file := os.Stdin
+	if len(settings.input) > 0 {
+		file, err = os.Open(settings.input)
+		if err != nil {
+			return
+		}
+		defer func(file *os.File) {
+			err = file.Close()
+			if err != nil {
+				return
+			}
+		}(file)
+	}
+	lines, err = readLines(file)
+	return
+}
+
+func readLines(reader io.Reader) (result []string, err error) {
 	scanner := bufio.NewScanner(reader)
 	for scanner.Scan() {
 		result = append(result, scanner.Text())
 	}
-	if err := scanner.Err(); err != nil {
-		return result, err
+	err = scanner.Err()
+	return
+}
+
+func writeData(settings ioSettings, data []string) (err error) {
+	file := os.Stdout
+	if len(settings.output) > 0 {
+		file, err = os.Create(settings.output)
+		if err != nil {
+			return
+		}
+		defer func(file *os.File) {
+			err = file.Close()
+			if err != nil {
+				return
+			}
+		}(file)
 	}
-	return result, nil
+	for _, str := range data {
+		_, err = file.WriteString(str)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func main() {
-	opts := parseCmd()
+	opts, settings := parseCmd()
 	if !opts.IsValid() {
 		fmt.Println("invalid arguments, usage:")
 		fmt.Println(correctOpts)
 		return
 	}
-	// debug
-	file, err := os.Open("./test_cases/test1.txt")
+	lines, err := readData(settings)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer func(file *os.File) {
-		err = file.Close()
-		if err != nil {
-			log.Fatal(err)
-		}
-	}(file)
-
-	lines, err := readLines(file)
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	output, err := uniq.Uniq(opts, lines)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	for _, str := range output {
-		fmt.Println(str)
+	err = writeData(settings, output)
+	if err != nil {
+		log.Fatal(err)
 	}
+	return
 }
